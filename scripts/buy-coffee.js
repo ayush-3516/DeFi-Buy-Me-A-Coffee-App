968519406c0c9fb43d795831d7b812e1322c17f7
ayush-3516 <letsmail.ayushchaudhari@gmail.com>
2022-08-06 18:23:51 +0530

    Update buy-coffee.js

--- scripts/buy-coffee.js
+++ scripts/buy-coffee.js
@@ -7,23 +7,57 @@ async function getBalance(address){
 }
 
 // Logs Ether balances for a list of addresses
-async function printBalance(addresses) {
+async function printBalances(addresses) {
     let idx = 0;
-    for (const address of addresses) 
-    [
-        console.log(`Address ${idx} balance: `, await getBalance(address));
-        idx++;
-    ]
-}
+    for (const address of addresses) {
+      console.log(`Address ${idx} balance: `, await getBalance(address));
+      idx ++;
+    }
+  }
 
+// Logs memos stored on-chain from coffee purchases  
 async function printMemos(memos) {
     for (const memo of memos) {
-        const timestamp 
+        const timestamp = memo.timestamp;
+        const tipper = memo.name;
+        const tipperAddress = memo.from;
+        const message = memo.message;
+        console.log(`At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}`);
     }
 }
 
 async function main(){
+    // Get example accounts
+    const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
+    
+    // Get contract to deploy
+    const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+    const buyMeACoffee = await BuyMeACoffee.deploy();
+    await buyMeACoffee.deployed();
+    console.log("BuyMeACoffee deployed to: ", buyMeACoffee.address);
 
+    // Deploy contract
+    const addresses = [owner.address, tipper.address, buyMeACoffee.address];
+    console.log("==Start==");
+    await printBalances(addresses);
+
+    // Check balances before coffee purchase
+    const tip = {value: hre.ethers.utils.parseEther("1")};
+    await buyMeACoffee.connect(tipper).buyCoffee("Sahil", "Bought you a Coffee, Keep up with the great stuff!!!", tip);
+    await buyMeACoffee.connect(tipper2).buyCoffee("Dinesh", "Keep posting great content!", tip);
+    await buyMeACoffee.connect(tipper3).buyCoffee("Dhairya", "This is a great dApp!!!", tip);
+
+    // Buy the owner a few coffees
+    console.log("== Bought Coffee ==");
+    await printBalances(addresses);
+
+    // Check balances after coffee purchase
+
+    // Withdraw funds
+
+    // Check balances after withdraw
+
+    // Read all the memos left for the owner
 }
 
 main()
